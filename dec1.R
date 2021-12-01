2989201a36a918b5c5e087a414464897b8b09073
Katelyn Schreyer <krschreyer@gmail.com>
2021-12-01 11:55:51 -0500

    Puzzle 2

---
+++ dec1.R
@@ -0,0 +1,26 @@
+# Advent of Code 2021
+# Dec 1
+# Puzzle #1
+
+# Count number of readings that are greater than the previous reading
+
+depths <- read_lines("input/dec1a.txt")
+
+depths <- as.integer(depths)
+
+differences <- diff(depths, lag = 1)
+
+length(differences[differences > 0])
+
+
+# Puzzle #2
+
+# create a three-measurement sliding window of sums, then find those that increase
+
+window_depths <- depths + lag(depths) + lag(depths, n = 2)
+
+window_depths <- na.omit(window_depths)
+
+window_diffs <- diff(window_depths, lag = 1)
+
+length(window_diffs[window_diffs > 0])
