0185ae6cd1f59840ee2dc7b1b679e5b2f29c3c32
Katelyn Schreyer <krschreyer@gmail.com>
2021-12-07 01:52:02 -0500

    cleaned up comments

--- dec7.R
+++ dec7.R
@@ -7,7 +7,7 @@ input <- str_split(read_lines("input/dec7.txt"),",") %>% unlist() %>% as.numeric
 
 # find horizontal position match that uses least number of steps ( wanna bet that part 2 adds vertical?)
 
-# is the median the ideal location? It is for the test set
+# Median is ideal location
 
 ideal_pos <- median(input)
 
@@ -16,12 +16,12 @@ fuel <- sum(abs(input - ideal_pos))
 # pt 2
 
 # lost the bet. fuel cost changes with steps
-# fuel cost increases with step
-# new ideal position is mean? 
+# Mean is ideal location
 
 fuel_cost <- function (steps) { steps*(steps+1)/2 }
 
 movement <- abs(input - as.integer(mean(input)))
+# R rounds to even. This works for my input but may be off-by-one for another input
 
 fuel_usage <- map(movement, fuel_cost) %>% unlist() %>% sum()
 
