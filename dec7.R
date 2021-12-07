71ad3f91b8968026c0a0f9bc7a39aa23272b9108
Katelyn Schreyer <krschreyer@gmail.com>
2021-12-07 01:20:11 -0500

    day 7 solutions

---
+++ dec7.R
@@ -0,0 +1,37 @@
+# Advent of Code 2021
+# Dec 7
+
+input <- str_split("16,1,2,0,4,2,7,1,2,14", ",") %>% unlist() %>% as.numeric()
+
+input <- str_split(read_lines("input/dec7.txt"),",") %>% unlist() %>% as.numeric()
+
+# find horizontal position match that uses least number of steps ( wanna bet that part 2 adds vertical?)
+
+# is the median the ideal location? It is for the test set
+
+ideal_pos <- median(input)
+
+fuel <- sum(abs(input - ideal_pos))
+
+# pt 2
+
+# lost the bet. fuel cost changes with steps
+# fuel cost increases with step
+# new ideal position is mean? 
+
+
+ideal_pos <- as.integer(mean(input)) 
+
+movement <- abs(input - ideal_pos)
+
+fuel_cost <- function (steps) {
+  fuel <- 0
+  while (steps > 0) {
+  fuel <- fuel + steps
+  steps = steps - 1
+  }
+  fuel
+}
+
+fuel_usage <- map(movement, fuel_cost) %>% unlist() %>% sum()
+
