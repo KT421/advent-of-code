101ded59b2bd2b257d7c7a36e10945d13b7f2627
Katelyn Schreyer <krschreyer@gmail.com>
2021-12-10 13:01:57 -0500

    today in Day 8 solutions: reinventing Breadth-First Search from first principals
    because I never took any compsci courses.

--- dec9.R
+++ dec9.R
@@ -9,7 +9,7 @@ input <- read_lines("2199943210
 8767896789
 9899965678") %>% str_split("") %>% unlist() %>% as.numeric() %>% matrix(ncol=10)
 
-input <- read_lines("input/dec9.txt") %>% str_split("") %>% unlist() %>% as.numeric() %>% matrix(ncol=100)
+input <- read_lines("input/dec9.txt") %>% str_split("") %>% unlist() %>% as.numeric() %>%  matrix(ncol=100, byrow = TRUE)
 
 # part 1 - sum all risk levels: local minima (adjacent only - no diags) + 1
 
@@ -61,7 +61,6 @@ sum(input[minima] + 1)
 # define a matrix of cells I have checked. Because 9s are not in basins, don't need to check them
 visited <- ifelse(input == 9, T, F)
 
-
 #store results in here
 basins <- tibble()
 
@@ -74,14 +73,16 @@ neighbors_in_bounds <- function(cell) {
   left <- cell - 1
   right <- cell + 1
   
-  if (up <= 0) up <- NA
-  if (down > length(input)) down <- NA
-  if (left %% 100 == 0) left <- NA
-  if ((right-1) %% 100 == 0) right <- NA
+  if (up <= 0 ) up <- NA
+  if (down > length(input) ) down <- NA
+  if ((left %% 100 == 0) ) left <- NA
+  if ((right-1) %% 100 == 0 ) right <- NA
   
-  cells_in_bounds <- c(up,down,left,right)
-  na.omit(cells_in_bounds)
+  cells_in_bounds <- na.omit(c(up,down,left,right))
+  cells_in_bounds <- cells_in_bounds[visited[cells_in_bounds] == FALSE]
+  cells_in_bounds
 }
+
 
 for (i in 1:length(input)) {
   
@@ -90,66 +91,55 @@ for (i in 1:length(input)) {
   # check to see if in an existing basin or is not in a basin
   if (visited[i] == TRUE) next
   
-  basin_id <- length(basins) + 1  
+  # create a new basin id
+  basin_size <- 0
+  basin_contents <- NULL
   
-  basin_size <- 1
-  basin_contents <- i
+  # starting from i, check neighbors
+  # add to checklist
+  # if valid, add to basin
+  # if list to be checked is length zero, break
+ 
+  cells_to_check <- i
+ 
+   while (length(cells_to_check) > 0) {
   
-  j <- T
-  up <- i
-  left <- i
-  right <- i
-  down <- i
-  
-  while (TRUE) {
-    while (up > 0) {
-      up <- up - 100
-      if (any(up <= 0,input[up] == 9)) {
-        up <- F 
-        break
-        }
-      basin_contents <- append(basin_contents,up)
-      basin_size <- basin_size + 1
-    }
-    while (down < length(input)) {
-      down <- down + 100
-      if (any(down > length(input), input[down] == 9)) {
-        down <- F 
-        break
-        }
-      basin_contents <- append(basin_contents,down)
-      basin_size <- basin_size + 1
-    }
-    while (left > 0) {
-      left <- left - 1
-      if (any(left %% 100 == 0, input[left] == 9,left<=0)) {
-        left <- F 
-        break
-        }
-      basin_contents <- append(basin_contents,left)
-      basin_size <- basin_size + 1
-    }
-    while (right < length(input) ) {
-      right <- right + 1
-      if (any((right-1) %% 100 == 0, right > length(input), input[right] == 9)) {
-        right <- F 
-        break
-        }
-      basin_contents <- append(basin_contents,right)
-      basin_size <- basin_size + 1
-    }
+     current_cell <- cells_to_check[1]
+     
+     # check if visited
+     if (visited[current_cell] == FALSE) {
+       # add to visited list 
+       visited[current_cell] <- TRUE
+     } 
+     
+       #check value
+       if (input[current_cell] != 9) {
+         #add neighbors to checklist
+         cells_to_check <- append(cells_to_check,neighbors_in_bounds(current_cell))
+         #add to basin
+         basin_contents <- append(basin_contents,current_cell)
+         #increment basin size
+         basin_size <- basin_size + 1
+       } 
+
+     
+     # remove this from checklist
+     cells_to_check <- cells_to_check[cells_to_check != current_cell]
+      
+     if (length(cells_to_check) == 0) break
   }
-    basin <- tibble(basin_id, basin_contents, basin_size)
+  
+  # add basin results to results table
+  basin_contents <- paste(basin_contents,collapse = ",")
+  
+    basin <- tibble(paste(basin_contents), basin_size)
     
     basins <- bind_rows(basins, basin)
 }
 
 
+# top 3 basins
 
-# testing while loops
-testing = 1
-while (testing != F) {
-  print(testing)
-  if (testing == 5) break
-  testing <- testing + 1
-}
+top_basins <- basins %>% arrange(basin_size) %>% top_n(3)
+
+top_basins$basin_size[1] * top_basins$basin_size[2] * top_basins$basin_size[3]
