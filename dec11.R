cc7475cc716fce826746ecb70d1e2fbb96c328b8
Katelyn Schreyer <krschreyer@gmail.com>
2021-12-11 08:12:10 -0500

    day 11 - cleanup
    merged pts 1 and 2 into the same loop

--- dec11.R
+++ dec11.R
@@ -21,11 +21,6 @@ test_input <- read_lines("5483143223
 
 octopi <- input
 
-increment_octopi <- function(octopi) {
-  octopi <- octopi + 1
-  octopi
-}
-
 neighbors_in_bounds <- function(octopus) {
   adjacent_octopi <- c(octopus - 10, octopus + 10)
   
@@ -33,59 +28,16 @@ neighbors_in_bounds <- function(octopus) {
   if ((octopus) %% 10 != 0) adjacent_octopi <- append(adjacent_octopi,c(octopus -9, octopus +1, octopus +11))
   
   adjacent_octopi <- adjacent_octopi[adjacent_octopi %in% 1:100]
-
-  adjacent_octopi
-  }
-
-flash_counter <- NULL
-
-for (steps in 1:100) {
-  octopi <- increment_octopi(octopi)
   
-  flashes <- ifelse(octopi > 9, "to flash", "waiting")
-
-  while (TRUE) {
-    
-    for (octopus in 1:length(flashes)) {
-      if (flashes[octopus] != "to flash") next
-      
-      adjacent_octopi <- neighbors_in_bounds(octopus)
-      
-      octopi[adjacent_octopi] <- increment_octopi(octopi[adjacent_octopi])
-      
-      # set octopi > 9 to "to flash" without popping any "flashed" to flash twice
-      
-      for (adjacent in adjacent_octopi) {
-        if (flashes[adjacent] != "flashed") {
-          if (octopi[adjacent] > 9) {
-            flashes[adjacent] <- "to flash"
-          }
-        }
-      }
-      
-      flashes[octopus] <- "flashed"
-    }
-    
-    if (sum(flashes == "to flash") == 0) break
-  }
-  flash_counter <- append(flash_counter,sum(flashes == "flashed"))
-  octopi[octopi > 9] <- 0
+  adjacent_octopi
 }
 
-sum(flash_counter)
-
-# part 2
-
-# find step when all are in sync
-
-# reset input
-octopi <- input
-
 step_counter <- 0
+flash_counter <- NULL
 
 while (TRUE) {
 
-  octopi <- increment_octopi(octopi)
+  octopi <- octopi + 1
   
   flashes <- ifelse(octopi > 9, "to flash", "waiting")
   
@@ -96,7 +48,7 @@ while (TRUE) {
       
       adjacent_octopi <- neighbors_in_bounds(octopus)
       
-      octopi[adjacent_octopi] <- increment_octopi(octopi[adjacent_octopi])
+      octopi[adjacent_octopi] <- octopi[adjacent_octopi] + 1
       
       # set octopi > 9 to "to flash" without popping any "flashed" to flash twice
       
@@ -117,7 +69,8 @@ while (TRUE) {
   octopi[octopi > 9] <- 0
   
   step_counter <- step_counter + 1
-  
+  flash_counter <- append(flash_counter,sum(flashes == "flashed"))
+  if (step_counter == 100) print((sum(flash_counter)))
   if (sum(flashes == "flashed") == 100) {
     print(step_counter)
     break
