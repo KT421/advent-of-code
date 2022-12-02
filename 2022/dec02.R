7f953fde0f57d25e6e436d375fb15acae3b9f739
Katelyn Schreyer <krschreyer@gmail.com>
2022-12-02 09:07:15 -0500

    Better solution for Day 2
    Use a lookup table instead of if statements for a cleaner solution

--- 2022/dec02.R
+++ 2022/dec02.R
@@ -1,55 +1,23 @@
 # AOC 2022 
 # Day 02
 
+# Solution V2 (using a lookup table - see commit history for clunky if statements)
+# it could be even shorter if I didn't convert ABC or XYZ to human words, but 
+# then I would get too confused. I'm already too confused.
+
 library(tidyverse)
 
 input <- readLines("2022/input/dec02.txt") %>% 
   as.data.frame() %>%
   separate(1, c("a","b"), sep = " ")
 
-# scoring function
-score_round <- function(winner, p2) {
-  score <- NULL
-  if (winner == "win") {
-    score <- 6
-  } else if (winner == "draw") {
-    score <- 3
-  } else {
-    score <- 0
-  }
-  
-  if (p2 == "rock") {
-    score <- score + 1
-  } else if (p2 == "paper") {
-    score <- score + 2
-  } else {
-    score <- score + 3
-  }
-  
-  return(score)
-}
-
-# determine winner
-
-play_rps <- function(p1, p2) {
-  
-  winner <- NULL 
-  
-  if (any(p1 == "rock" & p2 == "paper", 
-          p1 == "paper" & p2 == "scissors",
-          p1 == "scissors" & p2 == "rock")) {
-    winner <- "win"
-  } else if (any(p1 == "rock" & p2 == "scissors", 
-                 p1 == "paper" & p2 == "rock",
-                 p1 == "scissors" & p2 == "paper")) {
-    winner <- "lose"
-  } else {
-    winner <- "draw"
-  }
-  
-  #return result
-  return(winner)
-}
+# all possible results
+result_lookup <- data.frame(
+  p1 = c("rock","rock","rock","paper","paper","paper","scissors","scissors","scissors"),
+  p2 = c("rock","paper","scissors","rock","paper","scissors","rock","paper","scissors"),
+  winner = c("draw","win","lose","lose","draw","win","win","lose","draw"),
+  score = c(4,8,3,1,5,9,7,2,6)
+)
 
 # PT1
 
@@ -66,14 +34,14 @@ rps1 <- input %>%
     b == "Z" ~ "scissors"
   ))
 
-rps1 <- rps1 %>%
-  mutate(winner = mapply(play_rps, p1, p2),
-         score = mapply(score_round, winner, p2))
-
-sum(rps1$score)
+rps1 %>%
+  left_join(result_lookup, by = c("p1", "p2")) %>%
+  pull(score) %>%
+  sum()
 
 # PT2
 
+# interpret data
 rps2 <- input %>%
   transmute(p1 = case_when(
     a == "A" ~ "rock",
@@ -86,30 +54,8 @@ rps2 <- input %>%
     b == "Z" ~ "win"
   ))
 
-determine_winning_move <- function(p1, winner) {
-  
-  # determine winner
-  p2 <- NULL 
-  
-  if (any(p1 == "scissors" & winner == "win", 
-          p1 == "paper" & winner == "lose",
-          p1 == "rock" & winner == "draw")) {
-    p2 <- "rock"
-  } else if (any(p1 == "rock" & winner == "win", 
-                 p1 == "scissors" & winner == "lose",
-                 p1 == "paper" & winner == "draw")) {
-    p2 <- "paper"
-  } else if (any(p1 == "paper" & winner == "win", 
-         p1 == "rock" & winner == "lose",
-         p1 == "scissors" & winner == "draw")) {
-      p2 <- "scissors"
-    }
-    
-  return(p2)
-}
-
-rps2 <- rps2 %>%
-  mutate(p2 = mapply(determine_winning_move, p1, winner),
-         score = mapply(score_round, winner, p2))
-
-sum(rps2$score)
+# determine winning move
+rps2 %>%
+  left_join(result_lookup, by = c("p1", "winner")) %>%
+  pull(score) %>%
+  sum()
