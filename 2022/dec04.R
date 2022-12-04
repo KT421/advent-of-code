871cad0ef35e82b5f9224717e25681e6372a0083
Katelyn Schreyer <krschreyer@gmail.com>
2022-12-04 04:57:41 -0500

    cleaned up day 04

--- 2022/dec04.R
+++ 2022/dec04.R
@@ -1,5 +1,6 @@
 # AOC 2022
 # Day 04
+# v2 - much less repetitive 
 
 library(tidyverse)
 
@@ -7,36 +8,26 @@ input <- readLines("2022/input/dec04.txt") %>%
   str_replace_all("-",":") %>%
   strsplit(",") 
 
-
-# PT1
-
 find_overlap <- function(elf_assignments) {
   elf1 <- eval(parse(text = elf_assignments[[1]][1]))
   elf2 <- eval(parse(text = elf_assignments[[1]][2]))
   
-  ifelse(identical(intersect(elf1, elf2), elf1) || identical(intersect(elf1, elf2), elf2),T,F) 
+  subset <- ifelse(identical(intersect(elf1, elf2), elf1) || identical(intersect(elf1, elf2), elf2),T,F) 
+  overlap <- ifelse(is_empty(intersect(elf1,elf2)),F,T)
+  
+  return(c(subset,overlap))
 }
 
-overlaps <- NULL
+results <- NULL
+
 for (i in 1:1000) {
-  overlaps <- append(overlaps,find_overlap(input[i]))
+  results <- rbind(results,find_overlap(input[i]))
 }
 
-sum(overlaps)
+# PT1
+
+sum(results[,1])
 
 # PT2
 
-which_overlap <- function(elf_assignments) {
-  elf1 <- eval(parse(text = elf_assignments[[1]][1]))
-  elf2 <- eval(parse(text = elf_assignments[[1]][2]))
-  
-  ifelse(is_empty(intersect(elf1,elf2)),F,T)
-}
-
-overlaps <- NULL
-for (i in 1:1000) {
-  overlapping_sections <- which_overlap(input[i])
-  overlaps <- append(overlaps,overlapping_sections)
-}
-
-sum(overlaps)
+sum(results[,2])
